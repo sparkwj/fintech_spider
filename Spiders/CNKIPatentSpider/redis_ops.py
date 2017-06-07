4324c68300da208011ab91ca8e9619884c064c89
lxw0109 <lxw0109@gmail.com>
2017-06-07 21:48:28 +0800

    update cnki_patent_spider_wo_scrapy.py

---
+++ Spiders/CNKIPatentSpider/redis_ops.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: redis_ops.py
+# Author: lxw
+# Date: 6/7/17 3:07 PM
+
+import redis
+from Spiders.CNKIPatentSpider.settings import HOST
+from Spiders.CNKIPatentSpider.settings import PORT
+from Spiders.CNKIPatentSpider.settings import KEY_NAME
+
+
+class RedisClient:
+    pool = redis.ConnectionPool(host=HOST, port=PORT, db=0)
+    # [redis连接对象是线程安全的](http://www.cnblogs.com/clover-siyecao/p/5600078.html)
+    # [redis是单线程的](https://stackoverflow.com/questions/17099222/are-redis-operations-on-data-structures-thread-safe)
+    REDIS_URI = redis.Redis(connection_pool=pool)
+
+    def into_redis(self, field, value):
+        """
+        :param field: str
+        :param value: str
+        :return: None
+        """
+        self.REDIS_URI.hset(KEY_NAME, field, value)
+
+    def get_redis_uri(self):
+        return self.REDIS_URI
+
+    @classmethod
+    def get_redis_host(cls):
+        return HOST
+
+    @classmethod
+    def get_redis_key_name(cls):
+        return KEY_NAME
---
+++ Spiders/CNKIPatentSpider/settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# File: settings.py.py
+# Author: lxw
+# Date: 6/7/17 3:12 PM
+
+# Redis
+# HOST = "192.168.1.29"
+HOST = "127.0.0.1"
+PORT = 6379
+KEY_NAME = "CNKI_PATENT"
